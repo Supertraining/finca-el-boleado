cd8b0576c26e05cd2a9974b34ac04dc2c22f94bf
Matías Maranga <marangamatias@gmail.com>
2022-08-23 08:19:04 -0300

    desafio_n2

---
+++ src/components/NavBar.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+import logo from '../img/Logo_Boleado2.jpg'
+
+
+export const NavBar = () => {
+    return (
+    <>
+        <div className='w-auto d-flex align-items-center'>
+            <img src={logo} alt="logo" className='h-75' />
+        </div>
+        <ul className='list-unstyled w-auto d-flex'>
+            <li className='p-4'><a href='###' className='link-light text-decoration-none p-2'>Quienes somos</a></li>
+            <li className='p-4'><a href='###' className='link-light text-decoration-none p-2'>Nuestra historia</a></li>
+            <li className='p-4'><a href='###' className='link-light text-decoration-none p-2'>Carrito</a></li>
+        </ul>
+    </>
+    )
+}
